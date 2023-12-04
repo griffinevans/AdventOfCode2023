d86df89b963f2dd9c03535bed2b96b49e012a9d7
Griffin Evans <griffinevans@protonmail.com>
2023-12-03 23:53:56 -0800

    gitignore

--- day4/target/debug/Day4.d
+++
@@ -1,1 +0,0 @@
-/home/griffin/Projects/AdventOfCode2023/Day4/target/debug/Day4: /home/griffin/Projects/AdventOfCode2023/Day4/main.rs
--- day4/target/debug/deps/Day4-827cd699eef5a7bf.d
+++
@@ -1,5 +0,0 @@
-/home/griffin/Projects/AdventOfCode2023/Day4/target/debug/deps/Day4-827cd699eef5a7bf: main.rs
-
-/home/griffin/Projects/AdventOfCode2023/Day4/target/debug/deps/Day4-827cd699eef5a7bf.d: main.rs
-
-main.rs:
